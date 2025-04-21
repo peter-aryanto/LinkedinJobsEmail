16f17e141e726c971b82497acf8b51357c538de4
Peter Aryanto <peter.aryanto@gmail.com>
2025-04-21 16:38:29 +1000

    ymail tool - refactored code into a method

--- tools/ymail.py
+++ tools/ymail.py
@@ -5,71 +5,82 @@ import base64
 import os
 from imapclient import IMAPClient
 from email.header import decode_header, make_header  # This is built-in, NO need for `pip install`.
+from typing import List
 
 load_dotenv()
 
 YMAIL = os.getenv("YMAIL")
 YAPP_PASSWORD = os.getenv("YAPP_PASSWORD") or "matamu" # https://login.yahoo.com/account/security?.lang=en-US&.intl=us&.src=yhelp → https://login.yahoo.com/myaccount/security/?.lang=en-US&.intl=us&.src=yhelp&.scrumb=qCwrl830s2G&anchorId=appPasswordCard
 
-try:
-    with IMAPClient("imap.mail.yahoo.com", ssl=True) as mail:
-        mail.login(YMAIL, YAPP_PASSWORD)
 
-        mail.select_folder("INBOX")
+def readLinkedinJobsEmail() -> List[str]:
+    linkedJobUrls = []
 
-        messages = mail.search(["UNSEEN"])
-        messages = sorted(messages, reverse=True)
-        print(f"Unread Emails: {len(messages)}")
+    try:
+        with IMAPClient("imap.mail.yahoo.com", ssl=True) as mail:
+            mail.login(YMAIL, YAPP_PASSWORD)
 
-        # Fetch email headers
-        if messages:
-            linkedinJobsEmailCounter = 0
+            mail.select_folder("INBOX")
 
-            # for msg_id in messages[:3]:  # Limit to first 3 emails
-            for msg_id in messages[:12]:  # Limit to first 12 emails
-                msg_data = mail.fetch(msg_id, ["ENVELOPE"])
-                envelope = msg_data[msg_id][b'ENVELOPE']
+            messages = mail.search(["UNSEEN"])
+            messages = sorted(messages, reverse=True)
+            print(f"Unread Emails: {len(messages)}")
 
-                # sender = envelope.from_  # (Address(name=b'LinkedIn Job Alerts', route=None, mailbox=b'jobalerts-noreply', host=b'linkedin.com'),)
-                sender = envelope.from_[0]
+            # Fetch email headers
+            if messages:
+                linkedinJobsEmailCounter = 0
 
-                senderHost = sender.host.decode()
-                senderMailbox = sender.mailbox.decode()
+                # for msg_id in messages[:3]:  # Limit to first 3 emails
+                for msg_id in messages[:12]:  # Limit to first 12 emails
+                    msg_data = mail.fetch(msg_id, ["ENVELOPE"])
+                    envelope = msg_data[msg_id][b'ENVELOPE']
 
-                subjectraw = envelope.subject.decode().lower()
-                subject = str(make_header(decode_header(subjectraw)))
+                    # sender = envelope.from_  # e.g. "(Address(name=b'LinkedIn Job Alerts', route=None, mailbox=b'jobalerts-noreply', host=b'linkedin.com'),)"
+                    sender = envelope.from_[0]
 
-                # print()
-                # print(f"- From: {sender}")
-                # print(f"  Subject: {subject}")
+                    senderHost = sender.host.decode()
+                    senderMailbox = sender.mailbox.decode()
 
-                isLinkedJobsEmail = senderHost == "linkedin.com" and senderMailbox.startswith("job") \
-                    # and subject.startswith('“software developer”:')
-                if isLinkedJobsEmail:
-                    print()
-                    print(f"- From: {sender}")
-                    print(f"  Subject: {subject}")
-                    print(f"  {senderMailbox} - {senderHost}")
+                    subjectraw = envelope.subject.decode().lower()  # e.g. "=?UTF-8?Q?=E2=80=9Csoftware_developer=E2=80=9D:_Xero_-_Intermediate_Softw?= =?UTF-8?Q?are_Engineering_-_Product_&_Technology_and_more?=    "
+                    subject = str(make_header(decode_header(subjectraw)))
 
-                    if subject.startswith('“software developer”:'):
-                        linkedinJobsEmailCounter += 1
+                    # print()
+                    # print(f"- From: {sender}")
+                    # print(f"  Subject: {subject}")
 
-                    if linkedinJobsEmailCounter >= MAX_LINKEDIN_JOBS_EMAILS:
-                        break
+                    isLinkedJobsEmail = senderHost == "linkedin.com" and senderMailbox.startswith("job") \
+                        # and subject.startswith('“software developer”:')
+                    if isLinkedJobsEmail:
+                        print()
+                        print(f"- From: {sender}")
+                        print(f"  Subject: {subject}")
+                        print(f"  {senderMailbox} - {senderHost}")
+                        linkedJobUrls.append(subject)
 
-                # - From: (Address(name=b'LinkedIn Job Alerts', route=None, mailbox=b'jobalerts-noreply', host=b'linkedin.com'),)
-                #   Subject: =?UTF-8?Q?=E2=80=9Csoftware_developer=E2=80=9D:_Xero_-_Intermediate_Softw?= =?UTF-8?Q?are_Engineering_-_Product_&_Technology_and_more?=    
+                        if subject.startswith('“software developer”:'):
+                            linkedinJobsEmailCounter += 1
 
-        print()
+                        if linkedinJobsEmailCounter >= MAX_LINKEDIN_JOBS_EMAILS:
+                            break
 
-        exit(0)  #######
-        print('inner')
+            print()
 
-        # from email_reader import read_email_body, get_email_body
+            # exit(0)  #######
+            # print('inner')
 
-        # print(read_email_body(get_email_body(mail, messages[0])))
+            # from email_reader import read_email_body, get_email_body
 
-except Exception as e:
-    print("Error:", e)
+            # print(read_email_body(get_email_body(mail, messages[0])))
 
-print('outer')
+    except Exception as e:
+        print("Error:", e)
+
+    return linkedJobUrls
+
+
+if __name__ == "__main__":
+    linkedinJobUrls = readLinkedinJobsEmail()
+    print()
+    print("Linkedin URLs:")
+    for url in linkedinJobUrls:
+        print(url)
